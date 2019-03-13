c2b87e26e57aeb9e5272efe5afaf83079c995be1
Rogmer Bulaclac <rogmer.bulaclac@gmail.com>
2019-03-14 06:52:29 +0800

    latest commit - 03142019

--- sbtph/models/csdinbound.js
+++ sbtph/models/csdinbound.js
@@ -5,7 +5,7 @@ module.exports = class Inbound {
     static fetchAll(log,receive_calls) {
         const loginDirection = log
         const sql = `SELECT * FROM  csdinbound WHERE receive_calls=${receive_calls}`;
-       return database.execute(sql).then((rows, fields) => {
+        return database.execute(sql).then((rows, fields) => {
         return rows[0]
         
        }).then(async(rows) => {
@@ -21,8 +21,8 @@ module.exports = class Inbound {
        })
 
        function durationCalc(exten,login) {
-        const sql = `SELECT * FROM logs WHERE log='${login}' AND extension=${exten} ORDER by timestamp DESC LIMIT 1`
-        return database.execute(sql).then((rows,fields) => {
+            const sql = `SELECT * FROM logs WHERE log='${login}' AND extension=${exten} ORDER by timestamp DESC LIMIT 1`
+            return database.execute(sql).then((rows,fields) => {
             const lastLogin = rows[0][0].timestamp
     
             const getCurrentTimestamp = Date.now()
@@ -47,6 +47,7 @@ module.exports = class Inbound {
             return rows[0]
         })
     }
+
     static callSummary(date) {
         const getDate = date
         const sql = "SELECT * FROM csdinbound"
@@ -55,65 +56,54 @@ module.exports = class Inbound {
         }).then(async(rows) =>{
             let getCurrentrows = [...rows]
             for (let i=0;i<rows.length;i++){
-                await callDuration(getDate,rows[i].extension).then(summary => {
+                await this.callDuration(getDate,rows[i].extension).then(summary => {
                     getCurrentrows[i].summary = summary
                 })
 
             }
             return getCurrentrows
         }).catch(err => console.log(err))
-
-        function callDuration(date,extension) {
-            const sql = `SELECT StartTimeStamp,EndTimeStamp FROM inbound_callstatus WHERE getDate='${date}' AND CallStatus='ANSWER'  AND WhoAnsweredCall=${extension}`
-            return database.execute(sql).then((rows,fields) => {
-                return rows[0]
-            }).then(async(rows) => {
-                let summary = {}
-                let totalduration = [];
-                for (let i=0; i<rows.length; i++) {             
-                     totalduration[i] = await  (stringToTimeStamp(rows[i].EndTimeStamp) - stringToTimeStamp(rows[i].StartTimeStamp))
-                }
-                if (rows === 'undefined' || rows.length === 0){
-                    summary.totalduration = 0
-                   summary.totalCallAnswer = 0
-                }else{
-                 //add all elements in array
-                totalduration = totalduration.reduce((accumulator,currentValue) => accumulator + currentValue)
-                summary.totalduration = totalduration
-                summary.totalCallAnswer = rows.length
-               // console.log(summary)
-                }
-                
-                return summary
-            }).then(summary => {
-                const hours = Math.floor(summary.totalduration/1000/3600)
-                const minutes = Math.floor((summary.totalduration/1000/60) % 60)
-                const sec = Math.floor((summary.totalduration/1000) % 60)  
-        
-                const totalduration = `${hours}:${minutes}:${sec}`
-                 summary.totalduration = totalduration
-                return summary;
-            }).catch(err => console.log(err))
-        
-            function stringToTimeStamp(datestring){
-                //let separate = str.split("-")
-                let getyear = datestring.slice(0,4)
-                let getmonth = datestring.slice(4,6)
-                let getday = datestring.slice(6,8)
-                let gethours = datestring.slice(9,11)
-                let getmins = datestring.slice(11,13)
-                let getsec = datestring.slice(13)
-                
-                
-                let getdateAndTime = `${getyear}-${getmonth}-${getday} ${gethours}:${getmins}:${getsec}`
-                let timestamp = new Date(getdateAndTime)
-               // console.log( timestamp.getTime())
-                return timestamp.getTime()
-            }
-        }
-        
+      
     } 
-    static  getAgentCallDetails(extension, date) {
+
+    
+   static callDuration(date,extension) {
+        const sql = `SELECT StartTimeStamp,EndTimeStamp FROM inbound_callstatus WHERE getDate='${date}' AND CallStatus='ANSWER'  AND WhoAnsweredCall=${extension}`
+        return database.execute(sql).then((rows,fields) => {
+            return rows[0]
+        }).then(async(rows) => {
+            let summary = {}
+            let totalduration = [];
+            for (let i=0; i<rows.length; i++) {             
+                 totalduration[i] = await  (this.stringToTimeStamp(rows[i].EndTimeStamp).getTime - this.stringToTimeStamp(rows[i].StartTimeStamp).getTime)
+            }
+            //console.log(rows.length)
+            if (rows === 'undefined' || rows.length === 0){
+                summary.totalduration = 0
+                summary.totalCallAnswer = 0
+            }else{
+             //add all elements in array
+            totalduration = totalduration.reduce((accumulator,currentValue) => accumulator + currentValue)
+            summary.totalduration = totalduration
+            summary.totalCallAnswer = rows.length
+            console.log(summary)
+            }
+            
+            return summary
+        }).then(summary => {
+            const hours = Math.floor(summary.totalduration/1000/3600)
+            const minutes = Math.floor((summary.totalduration/1000/60) % 60)
+            const sec = Math.floor((summary.totalduration/1000) % 60)  
+    
+            const totalduration = `${hours}:${minutes}:${sec}`
+             summary.totalduration = totalduration
+            return summary;
+        }).catch(err => console.log(err))
+
+    }
+    
+    static getAgentCallDetails(extension, date) {
+       
        const  sql = `SELECT * FROM  inbound_callstatus WHERE WhoAnsweredCall=${extension} AND getDate='${date}' `
         return database.execute(sql).then((rows, fields) => {
             return rows[0]
@@ -122,50 +112,43 @@ module.exports = class Inbound {
             let totalduration;
             const getCurrentRows = [...rows]
             for(let i=0; i<rows.length; i++){
-              totalduration = await  (stringToTimeStamp(rows[i].EndTimeStamp).getTime - stringToTimeStamp(rows[i].StartTimeStamp).getTime)
+              totalduration = await  (this.stringToTimeStamp(rows[i].EndTimeStamp).getTime - this.stringToTimeStamp(rows[i].StartTimeStamp).getTime)
               const hours = Math.floor(totalduration/1000/3600)
               const minutes = Math.floor((totalduration/1000/60) % 60)
               const sec = Math.floor((totalduration/1000) % 60)  
         
               rows[i].totalduration = `${hours}:${minutes}:${sec}`
-              rows[i].startTime = stringToTimeStamp(rows[i].StartTimeStamp).toLocaleTimeString
-              rows[i].endTime = stringToTimeStamp(rows[i].EndTimeStamp).toLocaleTimeString
+              rows[i].startTime = this.stringToTimeStamp(rows[i].StartTimeStamp).toLocaleTimeString
+              rows[i].endTime = this.stringToTimeStamp(rows[i].EndTimeStamp).toLocaleTimeString
             }
             
             return rows
         }).catch(err => {
             console.log(err)
         })
-
-        function stringToTimeStamp(datestring){
-            const objectTime = {}
-            //convert format of timestamp from 20181204-010850 TO 2018-11-24 09:15:00
-            let getyear = datestring.slice(0,4)
-            let getmonth = datestring.slice(4,6)
-            let getday = datestring.slice(6,8)
-            let gethours = datestring.slice(9,11)
-            let getmins = datestring.slice(11,13)
-            let getsec = datestring.slice(13)
-            
-            
-            let getdateAndTime = `${getyear}-${getmonth}-${getday} ${gethours}:${getmins}:${getsec}`
-            let timestamp = new Date(getdateAndTime)
-       
-
-            objectTime.toLocaleTimeString = timestamp.toLocaleTimeString()
-            objectTime.getTime = timestamp.getTime()
-
-            return objectTime
-            
-        }
-
+        
     }
 
+   static stringToTimeStamp(datestring){
+        const objectTime = {}
+        //convert format of timestamp from 20181204-010850 TO 2018-11-24 09:15:00
+        let getyear = datestring.slice(0,4)
+        let getmonth = datestring.slice(4,6)
+        let getday = datestring.slice(6,8)
+        let gethours = datestring.slice(9,11)
+        let getmins = datestring.slice(11,13)
+        let getsec = datestring.slice(13)
+        
+        
+        let getdateAndTime = `${getyear}-${getmonth}-${getday} ${gethours}:${getmins}:${getsec}`
+        let timestamp = new Date(getdateAndTime)
+   
 
-  
+        objectTime.toLocaleTimeString = timestamp.toLocaleTimeString()
+        objectTime.getTime = timestamp.getTime()
 
-
-
-
+        return objectTime
+        
+    }
 
 }
